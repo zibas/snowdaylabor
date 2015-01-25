6f5a4bf0ba69f3571eebf8f019a5993b6223c93f
Ziba Scott <ziba@popcannibal.com>
2015-01-24 23:42:32 -0500

    various

--- Assets/SnowDayLabor/Scripts/Snowman.cs
+++ Assets/SnowDayLabor/Scripts/Snowman.cs
@@ -1,44 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Snowman : MonoBehaviour {
+public class Snowman : MonoBehaviour
+{
 
-	public BuildJobManager.CATEGORIES category;
-	public string description = "Cute, short name";
+		public BuildJobManager.CATEGORIES category;
+		public string description = "Cute, short name";
+		public SpriteRenderer original;
+		public SpriteRenderer recreated;
+		private int xChunkCount = 10;
+		private int yChunkCount = 10;
+		private int chunkWidth;
+		private int chunkHeight;
+		Texture2D newTex;
 
-	public SpriteRenderer original;
-	public SpriteRenderer recreated;
+		void Awake ()
+		{
 
-	void Awake(){
-
-		Texture2D tex = original.sprite.texture;
-		//Get hit position (placeholder)
-		//Vector2 pixelUV = hit.textureCoord;
-		//pixelUV.x *= tex.width;
-		//pixelUV.y *= tex.height;
+				Texture2D tex = original.sprite.texture;
+				//Get hit position (placeholder)
+				//Vector2 pixelUV = hit.textureCoord;
+				//pixelUV.x *= tex.width;
+				//pixelUV.y *= tex.height;
 
 
-		Texture2D newTex = new Texture2D (tex.width, tex.height, TextureFormat.ARGB32, false);
-		newTex.SetPixels32(tex.GetPixels32());
-		for(int x = 0; x < 50; x++){
-			for(int y = 0; y < 50; y++){
+				newTex = new Texture2D (tex.width, tex.height, TextureFormat.ARGB32, false);
 
-				newTex.SetPixel(x, y, Color.blue);
-			}
+				for (int x = 0; x < newTex.width; x++) {
+						for (int y = 0; y < newTex.height; y++) {
+								newTex.SetPixel (x, y, Color.clear);
+						}
+				}
+
+				chunkWidth = tex.width / xChunkCount;
+				chunkHeight = tex.height / yChunkCount;
+
+				for (int i = 0; i < 20; i++) {
+						DoChunk (i);
+				}
+				newTex.Apply ();
+				original.sprite = Sprite.Create (newTex, original.sprite.rect, new Vector2 (0.5f, 0.5f));
+
 		}
-		//newTex.SetPixel((int) pixelUV.x, (int) pixelUV.y, Color.black);
-		newTex.Apply();
-		original.sprite = Sprite.Create(newTex, original.sprite.rect, new Vector2(0.5f, 0.5f));
+
+		private void DoChunk (int chunk)
+		{
+				for (int x = (chunk%xChunkCount) * chunkWidth; x < ((chunk+1)%xChunkCount) * chunkWidth; x++) {
+					for (int y = (chunk%yChunkCount) * chunkHeight; y < ((chunk+1)%yChunkCount) * chunkHeight; y++) {
+						Color c = original.sprite.texture.GetPixel (x, y);
+									c.r += Random.Range (-10, 10);
+								newTex.SetPixel (x, y, c);
+					}
+				}
+				//newTex.SetPixel((int) pixelUV.x, (int) pixelUV.y, Color.black);
+				
+		}
 
 
-	}
-
-
-
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	}
+		}
 
 
 }
